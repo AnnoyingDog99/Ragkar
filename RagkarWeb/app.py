2f1014602a24f5d0d5fd94243f5f2859a66c7fce
Jitzek <jitz_k@hotmail.com>
2021-05-08 15:07:01 +0200

    stream

--- RagkarWeb/app.py
+++ RagkarWeb/app.py
@@ -1,12 +1,15 @@
-#!/usr/bin/python3
-
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+from src.stream import mjpeg_stream
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/stream')
+def stream():
+    return Response(mjpeg_stream(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0') 
